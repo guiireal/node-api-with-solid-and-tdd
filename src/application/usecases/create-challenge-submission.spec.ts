ed051d51115e6fe1f08f5d8253f704ecc0911e88
guiireal <guii_real@hotmail.com>
2022-02-15 00:51:18 -0300

    feat: upgrades

--- src/application/usecases/create-challenge-submission.spec.ts
+++ src/application/usecases/create-challenge-submission.spec.ts
@@ -1,8 +1,17 @@
+import { InMemoryChallengeRepository } from "../../../tests/repositories/in-memory-challenge-repository";
+import { InMemoryStudentRepository } from "../../../tests/repositories/in-memory-student-repository";
 import { CreateChallengeSubmission } from "./create-challenge-submission";
 
 describe("Create challenge submission use case", () => {
   it("should be able to create a new challenge submission", async () => {
-    const sut = new CreateChallengeSubmission();
+    const studentRepository = new InMemoryStudentRepository();
+    const challengeRepository = new InMemoryChallengeRepository();
+
+    const sut = new CreateChallengeSubmission(
+      studentRepository,
+      challengeRepository
+    );
+
     const response = await sut.execute({
       studentId: "fake-student-id",
       challengeId: "fake-challenge-id",
